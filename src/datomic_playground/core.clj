76c9fdb742a10bbe0aa4ce29bcef0be6bc854caf
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-03-21 14:47:24 +0100

    initial imprt

---
+++ src/datomic_playground/core.clj
@@ -0,0 +1,88 @@
+(ns datomic-playground.core
+  [:require [datomic.api :as d]])
+
+(def uri "datomic:mem://koalite")
+
+;; Crea una base de datos, borrando la que pudiera existir antes
+(defn reset-db []
+  (d/delete-database uri)
+  (d/create-database uri))
+
+;; Añado un atributo :hero/name. En la misma transacción se define el
+;; atributo (que es una entidad más y se define en base a sus propios
+;; atributos) y se "instala" en la base de datos, que en realidad es
+;; asignarlo como valor dentro del atributo :db.install/attribute de
+;; la entidad identificada por :db.part/db (creo que había un tipo
+;; especial de atributo que servía como identificador de entidades y
+;; se podía usar en lugar del id
+
+(defn add-schema [conn]
+  (d/transact
+   conn
+   [{:db/id #db/id[:db.part/db -1]
+     :db/ident :hero/name
+     :db/valueType :db.type/string
+     :db/cardinality :db.cardinality/one
+     :db/doc "El nombre del héroe"}
+    [:db/add :db.part/db :db.install/attribute #db/id[:db.part/db -1]]]))
+
+(defn add-heroes [conn]
+  (d/transact
+   conn
+   [{:db/id #db/id[:db.part/user] :hero/name "Macareno"}
+    {:db/id #db/id[:db.part/user] :hero/name "Luciano"}]))
+
+(defn add-heroe [conn name]
+  (d/transact
+   conn
+   [{:db/id #db/id[:db.part/user] :hero/name name}]))
+
+(defn get-heroes [dbval]
+  (let [heroes (d/q '[:find ?e :where [?e :hero/name]] dbval)]
+    (map #(d/touch (d/entity dbval (first %))) heroes)))
+
+
+(reset-db)
+
+(def conn (d/connect uri))
+
+(add-schema conn)
+
+(add-heroe conn "Macareno")
+(add-heroe conn "Luciano")
+
+;; Añado un nuevo atributo al esquema
+(d/transact
+  conn
+  [{:db/id #db/id[:db.part/db -1]
+    :db/ident :hero/age
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc "La edad"}
+   [:db/add :db.part/db :db.install/attribute #db/id[:db.part/db -1]]])
+
+;; Busco a Macareno para ponerle una edad
+
+(def dbval (d/db conn))
+
+(def macareno (ffirst (d/q '[:find ?e :where [?e :hero/name "Macareno"]] dbval)))
+
+(def macareno-entity (d/entity dbval macareno))
+
+;; Para ponerle edad, no debería necesitar ni siquiera el entity
+
+(d/transact
+ conn
+ [{:db/id macareno
+   :hero/age 1723}])
+
+;; Ahora si consulto con el dbval anterior, no debería existir la edad
+;; de macareno (esa es la idea de db as value, ¿no?)
+
+(d/touch (d/entity dbval macareno)) ;; :db/id y :hero/name
+
+;; Si consulto con la base de datos nueva debería aparecer
+(d/touch (d/entity (d/db conn) macareno)) ;; efectivamente!!!
+
+
+
